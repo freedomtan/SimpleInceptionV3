12318a9f8caeaa4717b36bcefd6dcc3ad403dfcd
Koan-Sin Tan <koansin.tan@gmail.com>
2017-06-11 11:18:37 +0800

    Initial Commit

---
+++ SimpleInceptionV3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleInceptionV3
+//
+//  Created by Koan-Sin Tan on 6/11/17.
+//  Copyright © 2017 MediaTek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
