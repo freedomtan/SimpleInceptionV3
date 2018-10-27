4be78da6d8fae104b4f35b2b16bcf58cb7015413
Koan-Sin Tan <koansin.tan@gmail.com>
2018-10-27 10:42:13 +0800

    upgrade to Swift 4.2 and iOS 12

--- SimpleInceptionV3/ViewController.swift
+++ SimpleInceptionV3/ViewController.swift
@@ -36,8 +36,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var modelConfig: MLModelConfiguration
+        modelConfig = MLModelConfiguration()
+        modelConfig.computeUnits = MLComputeUnits.cpuAndGPU
+        // default is modelConfig.computeUnits = MLComputeUnits.all
 
-        model = try! VNCoreMLModel(for: MobileNet_050_160().model)
+        model = try! VNCoreMLModel(for: MobileNet_050_160(configuration: modelConfig).model)
         request = VNCoreMLRequest(model: model) { [weak self] request, error in
             let stopTicks = Date().timeIntervalSince1970
             let startTicks = self!.startTimes.remove(at: 0)
@@ -66,10 +70,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         // Dispose of any resources that can be recreated.
     }
     
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+// Local variable inserted by Swift 4.2 migrator.
+let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+
         picker.dismiss(animated: true)
         messageLabel.text = "Analyzing Image…"
-        guard let uiImage = info[UIImagePickerControllerOriginalImage] as? UIImage
+        guard let uiImage = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as? UIImage
             else { fatalError("no image from image picker") }
         
         // Show the image in the UI.
@@ -171,3 +178,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
 }
 
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+	return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+	return input.rawValue
+}
